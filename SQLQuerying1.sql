9d3f5f9f04982103a95d4faa2d437ee89151b77d
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-27 07:29:50 -0600

    Create SQLQuerying1.sql

---
+++ SQLQuerying1.sql
@@ -0,0 +1,10 @@
+/* Questions What year saw the highest immigrants obtain permanent resident status? */ 
+
+SELECT 
+  year, 
+  Immigrants_Obtaining_Lawful_Permanent_Resident_Status
+FROM
+  `celestial-feat-399513.US_Immigration.us_immigration`
+ORDER BY Immigrants_Obtaining_Lawful_Permanent_Resident_Status DESC
+LIMIT 1 
+
