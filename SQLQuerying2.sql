19606cc7a569c30df8239c81ea91a290868275d5
rvillanu7 <143353548+rvillanu7@users.noreply.github.com>
2023-11-27 08:09:17 -0600

    SQLQuerying2.sql

--- SQLQuerying2.sql
+++ SQLQuerying2.sql
@@ -1,14 +1,13 @@
 /* On average, where there more immigrants obtaining permanent residency or refugee arrivals? */ 
 SELECT 
-  MAX(Immigrants_Obtaining_Lawful_Permanent_Resident_Status) AS max_permanentresidency,
-  MAX(Refugee_Arrivals) AS max_refugeearrivals
+  ROUND(AVG(Immigrants_Obtaining_Lawful_Permanent_Resident_Status)) AS avg_permanentresidency,
+  ROUND(AVG(Refugee_Arrivals)) AS avg_refugeearrivals
 FROM
   `celestial-feat-399513.US_Immigration.us_immigration`
 
 
 /*BONUS- How many more times are there Immigrants obtaining Lawful Permanent Resident Status than Refugee Arrivals?*/
 SELECT 
-  ROUND(MAX(Immigrants_Obtaining_Lawful_Permanent_Resident_Status) /
-  MAX(Refugee_Arrivals)) AS amt_more_p_residency
+  ROUND(AVG(Immigrants_Obtaining_Lawful_Permanent_Resident_Status) / (AVG(Refugee_Arrivals))) AS amt_more_p_residency
 FROM
   `celestial-feat-399513.US_Immigration.us_immigration`
